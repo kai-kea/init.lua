2c3f6fadf62a149f890bebf7171e706c5ee9a6a4
kai-kea <julianktfaldridge@icloud.com>
2024-03-29 04:59:34 -0400

    refactor: extract custom plugins

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -188,38 +188,6 @@ require('lazy').setup({
     end,
   },
 
-  {
-    'nvim-java/nvim-java',
-    dependencies = {
-      'nvim-java/lua-async-await',
-      'nvim-java/nvim-java-core',
-      'nvim-java/nvim-java-test',
-      'nvim-java/nvim-java-dap',
-      'MunifTanjim/nui.nvim',
-      'neovim/nvim-lspconfig',
-      'mfussenegger/nvim-dap',
-      {
-        'williamboman/mason.nvim',
-        opts = {
-          registries = {
-            'github:nvim-java/mason-registry',
-            'github:mason-org/mason-registry',
-          },
-        },
-      },
-      {
-        'williamboman/mason-lspconfig.nvim',
-        opts = {
-          handlers = {
-            ['jdtls'] = function()
-              require('java').setup()
-            end,
-          },
-        },
-      },
-    },
-  },
-
   { -- LSP Configuration & Plugins
     'neovim/nvim-lspconfig',
     dependencies = {
@@ -659,28 +627,8 @@ require('lazy').setup({
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
   -- {
-  --   import = '',
+  --   import = 'custom.plguins',
   -- },
-
-  {
-    'kawre/leetcode.nvim',
-    build = ':TSUpdate html',
-    dependencies = {
-      'nvim-telescope/telescope.nvim',
-      'nvim-lua/plenary.nvim', -- required by telescope
-      'MunifTanjim/nui.nvim',
-
-      -- optional
-      'nvim-treesitter/nvim-treesitter',
-      -- 'rcarriga/nvim-notify',
-      'nvim-tree/nvim-web-devicons',
-    },
-    opts = {
-      ---@type lc.lang
-      lang = 'python3',
-      -- configuration goes here
-    },
-  },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
